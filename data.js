91a871769a731f691a201ed6832386a944c79d76
阎魔爱 <8@9i3.cn>
2018-01-27 13:51:31 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,72 @@
+var transform = {'tag':'li','html':'${link}'};  
+  
+var data = [  
+      {'link':'<a href="https://share.weiyun.com/ed7cdefae8088cf401e638d410e6bf4b" target="_blank" class="am-list-item-hd ">花江夏树.mp3</a>'},  
+      {'link':'<a href="https://share.weiyun.com/b4720c99a3d2e5a37f235411588c773f" target="_blank" class="am-list-item-hd ">BLCD HARD (need headphones!).mp3</a>'}, 
+      {'link':'<a href="https://share.weiyun.com/ecb899d84dcb664d12e6bc2e0f6fe4ea" target="_blank" class="am-list-item-hd ">Ultimate BLCD.mp3</a>'},  
+      {'link':'<a href="https://share.weiyun.com/f2704af607b480b6c6f18ece23c0dbea" target="_blank" class="am-list-item-hd ">檜山修之×櫻井孝宏.mp3</a>'}, 
+      {'link':'<a href="https://share.weiyun.com/866f5d1b81ef66b68c1ae6a14c57d79b" target="_blank" class="am-list-item-hd ">三木眞一郎×神谷浩史.mp3</a>'},  
+      {'link':'<a href="https://share.weiyun.com/928dc776c46fa619cc678749f2e370fe" target="_blank" class="am-list-item-hd ">神谷浩史X梶裕贵.mp3</a>'}, 
+      {'link':'<a href="https://share.weiyun.com/cc037e15483b84d0ac885f3fce78acde" target="_blank" class="am-list-item-hd ">【中抓】強行play H</a>'}, 
+      {'link':'<a href="https://share.weiyun.com/1de4c7d2d747ebc564e64313dd791111" target="_blank" class="am-list-item-hd ">学姐分享 1- 3 弹 合集</a>'}, 
+      {'link':'<a href="https://share.weiyun.com/38b56a75739b76e38f147e172b6a65cd" target="_blank" class="am-list-item-hd ">学姐分享 4- 6 弹 合集</a>'}, 
+      {'link':'<a href="https://share.weiyun.com/f273a40a06672d9956f0cb7c7b362f7d" target="_blank" class="am-list-item-hd ">学姐分享 7- 9 弹 合集</a>'}, 
+      {'link':'<a href="https://share.weiyun.com/c92e0c4d070c2f7923d13b1f5792fd77" target="_blank" class="am-list-item-hd ">学姐分享 10- 12 弹 合集</a>'}, 
+      {'link':'<a href="https://share.weiyun.com/f381c899224a14a0d0022b4a1dcae5c3" target="_blank" class="am-list-item-hd ">【BL界の女王】石田彰</a>'}, 
+      {'link':'<a href="https://share.weiyun.com/b488c2dcf975b289f4ebfab54a4ff035" target="_blank" class="am-list-item-hd ">学姐分享 13- 15 弹 合集</a>'}, 
+      {'link':'<a href="https://share.weiyun.com/678047938f2897203805d354bdb0f565" target="_blank" class="am-list-item-hd ">学姐分享 单P6弹 17/18弹被删除</a>'}, 
+      {'link':'<a href="https://share.weiyun.com/919f42805b8c992da72e0030afb88500" target="_blank" class="am-list-item-hd ">学姐分享 19- 22 弹 合集</a>'}, 
+      {'link':''}, 	  
+      {'link':''}, 
+      {'link':''}, 
+      {'link':''}, 
+      {'link':''}, 	  
+      {'link':''}	  
+];  
+  
+document.getElementById('cd').innerHTML = json2html.transform(data,transform);  
+
+var transform = {'tag':'li','html':'${link}'};  
+  
+var data = [  
+      {'link':'<a href="https://share.weiyun.com/9b96668b047960ba67a921a25ddba6db" target="_blank" class="am-list-item-hd ">BL封面合集</a>'}, 	  
+      {'link':'<a href="https://pan.baidu.com/s/1nvK14Tj" target="_blank" class="am-list-item-hd ">【日高ショーコ】忧郁な朝（忧郁的早晨)</a>'}, 
+      {'link':'<a href="https://share.weiyun.com/ca58feef832d7431d649d040fb8bbe10" target="_blank" class="am-list-item-hd ">[绪川千世]格差天堂</a>'}, 
+      {'link':'<a href="https://pan.baidu.com/s/1c2MfAX6" target="_blank" class="am-list-item-hd ">[宝井理人][TEN COUNT][單行本3加筆漫畫]</a>'}, 
+      {'link':'<a href="https://pan.baidu.com/s/1bYKoxC" target="_blank" class="am-list-item-hd ">[只有花知晓]宝井理人</a>'}, 	 
+      {'link':'<a href="https://pan.baidu.com/s/1i4KgQEh" target="_blank" class="am-list-item-hd ">忘却之梦</a>'}, 	
+      {'link':'<a href="https://pan.baidu.com/s/1nueDSCT" target="_blank" class="am-list-item-hd "> 家庭教师同人10本BL.zip</a>'}, 	 
+      {'link':''}, 		  
+      {'link':''}, 	 
+      {'link':''}, 	  
+      {'link':''}	  
+];  
+  
+document.getElementById('mh').innerHTML = json2html.transform(data,transform);  
+
+var transform = {'tag':'li','html':'${link}'};  
+  
+var data = [  
+      {'link':'<a href="https://pan.baidu.com/s/1i4KgtLF" target="_blank" class="am-list-item-hd ">bl文（主要是主攻文）.zip</a>'}, 	  
+      {'link':'<a href="https://pan.baidu.com/s/1bp8x55X" target="_blank" class="am-list-item-hd ">搞笑BL文.zip</a>'}, 
+      {'link':'<a href="https://pan.baidu.com/s/1kUufuir" target="_blank" class="am-list-item-hd ">公事攻办.txt</a>'}, 
+      {'link':'<a href="https://share.weiyun.com/72c6b11a341be7b4697b84d7e3484a03" target="_blank" class="am-list-item-hd ">攻很好哄の文</a>'}, 
+      {'link':'<a href="https://share.weiyun.com/47225a2f6cdcb22c2780bbf517342e6d" target="_blank" class="am-list-item-hd ">先上床再恋爱</a>'}, 	
+      {'link':''}, 		  
+      {'link':''}	  
+];  
+  
+document.getElementById('xs').innerHTML = json2html.transform(data,transform);  
+
+var transform = {'tag':'li','html':'${link}'};  
+  
+var data = [  
+      {'link':'<a href="https://pan.baidu.com/s/1eRF6YsE" target="_blank" class="am-list-item-hd "> 百日蔷薇</a>'}, 	  
+      {'link':'<a href="https://pan.baidu.com/s/1qYIkqz6" target="_blank" class="am-list-item-hd ">探索者系列</a>'}, 
+      {'link':''}, 
+      {'link':''}, 
+      {'link':''}, 	  
+      {'link':''}	  
+];  
+  
+document.getElementById('dm').innerHTML = json2html.transform(data,transform);  
